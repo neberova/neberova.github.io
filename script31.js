ccc63142b88d67b370db959f2ea5e17330d4eb85
neberova <39513682+neberova@users.noreply.github.com>
2018-10-25 13:48:13 +0300

    Create script31.js

---
+++ script31.js
@@ -0,0 +1,25 @@
+function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f2()
+{
+        a=parseFloat($('tx1').value);
+	b=parseFloat($('tx2').value);
+        x1=a;
+        x2=b;
+	if(a>b)
+{$('tx3').value=''+a;
+}
+else
+{$('tx3').value=''+b;
+}
+}
