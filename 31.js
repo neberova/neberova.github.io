6e2e371a17836b891b55b193409660fadbc66845
neberova <39513682+neberova@users.noreply.github.com>
2018-12-17 19:36:43 +0300

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,22 @@
+function middle() {
+var a = parseInt(document.getElementById('a').value);
+var price = prompt('Укажите цену за 1 кг.');
+        var res;
+        var weight;
+        price = parseFloat(price.replace(/,/, '.'));
+
+        if(isNaN(price) == true) {
+            alert('Цена должна быть вещественным числом!');
+            location.reload();
+        } else {
+            if ((typeof price == 'number')) {
+                for (var i = 1; i <= 10; i++){
+                    weight = i/10;
+                    res = weight * price;
+                    alert('Цена за ' + weight + ' кг равна ' + res + ' грн.');
+                }
+            } else {
+                alert('Цена должна быть вещественным числом!');
+                location.reload();
+            }
+        }
