59c4ce11a7103e65f063e949ecda34c3bfc2c4dc
neberova <39513682+neberova@users.noreply.github.com>
2018-12-19 00:32:22 +0300

    Create l33.js

---
+++ l33.js
@@ -0,0 +1,35 @@
+function $() 
+{
+var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements;}
+
+function f1()
+{
+	a = parseInt($('tx1').value);
+	b = parseInt($('tx2').value);
+	if (a<b) 
+	{
+		var i;
+		var step;
+		v = '';
+		for (i = a; i <= b; i++) 
+		{		
+			for (step = 1; step <= i; step++)
+			{
+				v=v+i+'.';
+			}
+		}
+		$('tx3').value=v;
+	}
+	else
+	{
+		$('tx3').value='A should be LESS than B!';
+	}
