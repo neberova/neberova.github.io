3645159e8a463b26244d93fc37f5a849e6685a4e
neberova <39513682+neberova@users.noreply.github.com>
2019-01-14 21:17:32 +0300

    Create l6.js

---
+++ l6.js
@@ -0,0 +1,28 @@
+ function makeEaseOut(timing) {
+      return function(timeFraction) {
+        return 1 - timing(1 - timeFraction);
+      }
+    }
+
+    function bounce(timeFraction) {
+      for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+    }
+
+    ball.onclick = function() {
+
+      var to = field.clientHeight - ball.clientHeight;
+
+      animate({
+        duration: 2000,
+        timing: makeEaseOut(bounce),
+        draw: function(progress) {
+          ball.style.top = to * progress + 'px'
+        }
+      });
+
+
+    };
